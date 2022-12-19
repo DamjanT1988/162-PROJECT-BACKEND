b91c919a6296615bac98ad2717935631f1ad0e3c
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-19 01:24:28 +0100

    1

--- routes/keys.js
+++ routes/keys.js
@@ -1,4 +1,3 @@
-
 //require express, create router
 var express = require('express');
 var router = express.Router();
@@ -28,7 +27,6 @@ db.once('open', function (callback) {
 	//create a model
 	var Key = mongoose.model('Key', keySchema)
 
-
 	/********************************************* 
 	 * Get complete listing
 	 *********************************************/
@@ -36,8 +34,9 @@ db.once('open', function (callback) {
 		//read from Mongo database
 		Key.find(function (err, keyMongo) {
 			if (err) return console.error(err);
+			//save Mongo data
 			var key = keyMongo;
-
+			//return JSON data
 			var jsonObj = JSON.stringify(key);
 			res.contentType('application/json');
 			res.send(jsonObj);
@@ -47,23 +46,24 @@ db.once('open', function (callback) {
 	/********************************************* 
 	 * Get unique id
 	 *********************************************/
-	
 	router.get('/:id', function (req, res, next) {
-		//read from Mongo database
+		//find and read from Mongo database
 		Key.find(function (err, keyMongo) {
 			if (err) return console.error(err);
+			//save Mongo data
 			var key = keyMongo;
-
 			//read in mongoDB special id numbers
 			var id = req.params.id;
 			var ind = -1;
-
+			//loop id numbers
 			for (var i = 0; i < key.length; i++) {
 				//find the array index that holds _id = id
 				if (key[i]._id == id) ind = i;    
 			}
+			//return JSON data
 			res.contentType('application/json');
-			//if we find the user id then return the user object otherwise return {}
+			//if we find the user id then return the user 
+			//object otherwise return {}
 			res.send(ind >= 0 ? key[ind] : '{}'); 
 		});
 	});
@@ -74,16 +74,16 @@ db.once('open', function (callback) {
 	router.put('/:id', function (req, res, next) {
 		Key.find(function (err, keyMongo) {
 			if (err) return console.error(err);
-			var key = keyMongo;
+			//save id
 			var id = req.params.id;
-			
+			//save JSON call body data
 			var body = req.body;
 
-			//send back key
+			//send back information
 			res.contentType('application/json');
 			res.send(id + " key updated!");
 
-			//remove post in database
+			//remove post/object in database
 			Key.updateOne({ _id: id }, {$set: body}, function (err, result) {
 				if (err) {
 					console.log(err)
--- routes/users.js
+++ routes/users.js
@@ -47,6 +47,65 @@ db.once('open', function (callback) {
 	});
 
 	/********************************************* 
+	 * Update password
+	 *********************************************/
+		router.put('/:id', function (req, res, next) {
+			User.find(function (err, userMongo) {
+				if (err) return console.error(err);
+				
+				var id = req.params.id;
+				var body = req.body;
+	
+				//send back key
+				res.contentType('application/json');
+				res.send(id + " password updated!");
+	
+				var id = req.params.id;
+				User.updateOne({ _id: id }, {$set: body}, function (err, result) {
+					if (err) {
+						console.log(err)
+					} else {
+						console.log("Result :", result)
+					}
+				});
+			});
+		});
+
+	/********************************************* 
+	 * Delete unique user
+	 *********************************************/
+		router.delete('/:id', function (req, res, next) {
+			User.find(function (err, userMongo) {
+				if (err) return console.error(err);
+				var user = userMongo;
+	
+				var id = req.params.id;
+				var del = -1;
+	
+				//fix the array list
+				for (var i = 0; i < user.length; i++) {
+					//find the array index that holds _id = id
+					if (user[i]._id == id) del = i;     
+				}
+				//delete element and fix array
+				if (del >= 0) status = user.splice(del, 1); 
+	
+				//send back id
+				res.contentType('application/json');
+				res.send(id + " user deleted");
+	
+				//remove post in database
+				User.deleteOne({ _id: id }, function (err, result) {
+					if (err) {
+						console.log(err)
+					} else {
+						console.log("Result :", result)
+					}
+				});
+			});
+		});
+
+	/********************************************* 
 	 * Add new user
 	 *********************************************/
 	router.post('/', function (req, res, next) {
@@ -67,32 +126,6 @@ db.once('open', function (callback) {
 			//save to mongodb
 			newUser.save(function (err) {
 				if (err) return console.error(err);
-			});
-		});
-	});
-
-	/********************************************* 
-	 * Update password
-	 *********************************************/
-	router.put('/:id', function (req, res, next) {
-		User.find(function (err, userMongo) {
-			if (err) return console.error(err);
-			//var key = userMongo;
-			var id = req.params.id;
-			
-			var body = req.body;
-
-			//send back key
-			res.contentType('application/json');
-			res.send(id + " password updated!");
-
-			//remove post in database
-			Key.updateOne({ _id: id }, {$set: body}, function (err, result) {
-				if (err) {
-					console.log(err)
-				} else {
-					console.log("Result :", result)
-				}
 			});
 		});
 	});
