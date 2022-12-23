f5420de039e41178f2bf8462f2bb027d71cf6321
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-23 05:18:52 +0100

    1

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+var cors = require('cors');
 
 //declare pathway to file; get files
 var indexRouter = require('./routes/index');
